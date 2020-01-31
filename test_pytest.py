4d29e18f1dd58aa076edd1fd1a4b4e532dfb8dad
Andrey22 <aandreyko@mail.ru>
2020-02-01 02:19:23 +0300

    branch

--- test_pytest.py
+++ test_pytest.py
@@ -18,6 +18,68 @@ new_list = choice_name(listnames, length=100)
 
 print(new_list)
 
+def frequentname(listname):
+
+    dict = {}
+
+    for name in listname:
+
+        dict[name] = dict.get(name,0) + 1
+
+    dict = list(dict.items())
+
+    dict.sort(key=lambda x: x[1], reverse=True)
+
+    # x[1]  означает сортировку по Values, т.е. по количеству повторений имени в словаре (имя, количество повторений)
+
+    # [0][0]  = вывод ключа первого элемента из отсортированного словаре dict,
+
+    # [0][1] = вывод ключа второго элемента из отсортированного словаре dict
+
+    # [1][0] = вывод Value первого элемента из отсортированного словаре dict
+
+    return dict[0][0]
+
+
+
+print(frequentname(new_list))
+
+
+
+# 3. Напишите функцию вывода самой редкой буквы, с которого начинаются имена в списке на выходе функции F.
+
+
+
+def rareletter(list2):
+
+    dict2 = {}
+
+    for name in list2:
+
+        for char in name:
+
+            #
+
+            # выявление заглавной буквы в имени
+
+            #
+
+            if char.isupper():
+
+                dict2[char] = dict2.get(char, 0) + 1
+
+            else: continue
+
+    dict2 = list(dict2.items())
+
+    dict2.sort(key=lambda x: x[1])
+
+    return dict2[0][0]
+
+# return dict2[0][0], dict2[1][0], dict2, dict2[0][0].isupper()
+
+print(rareletter(new_list))
+
 def test1():
     assert len(choice_name(listnames, length=100 ))==100
     # assert len(new_list) == 100
